8f3cfacbf984c0147dd03b9d2fc72c24d4658ba7
ASUS <ASUS@ASUS-TUF-F15>
2024-10-17 19:54:30 +0800

    Add project files.

---
+++ YoutubeApiProject/Models/YoutubeVideoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace YoutubeApiProject.Models
+{
+    public class YoutubeVideoModel
+    {
+        public string VideoId { get; set; }  // Add this line to store the Video ID
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ThumbnailUrl { get; set; }
+        public string VideoUrl { get; set; } // Add this property for the video link
+    }
+}
+
